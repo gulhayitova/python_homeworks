2b9fcfc2a1a5ffed68d0db0925487719a085f687
gulhayitova <gulhayohayitova07@gmail.com>
2025-02-14 00:40:52 +0500

    Final changes

--- lesson-3/homework/dictionary1.py
+++ lesson-3/homework/dictionary1.py
@@ -76,10 +76,42 @@ for i in range(len(keys)):
     newDict.update({keys[i]:values[i]})
 print(newDict)
 
+newDict[10] = {1:2,2:3}
+#16 check nested dictionaries
+for val in newDict.values():
+    if type(val)==dict:
+        print("The dictionary has another dictionary value.")
+        #17 get a value from it. I got the first value here
+        print(list(val.values())[0])
+        break
 
+#18 default value for missing keys
+from collections import defaultdict
+newDict = defaultdict(lambda: "No value for this key")
 
+#19 count unique values
+m_values = list(merged.values())
+print(len(set(m_values)))
 
+#20 sort by key
+print(dict(sorted(merged.items())))
 
+#21 sort by values
+print(dict(sorted(merged.items(), key=lambda item: item[1])))
 
+#22 filter by value
+m_even = {x:y for x, y in merged.items() if x%2 == 0}
 
+#23 check for common keys
+set1 = list(m_even.keys())
+set2 = list(merged.keys())
+set1, set2 = set(set1), set(set2)
+print(set1&set2)
 
+#24 tuple to dict
+tuple1 = ((1,"a"),(2,"b"),(3,"c"))
+print(dict(tuple1))
+
+#25 get the first pair
+tuple1 = dict(tuple1)
+print(list(tuple1.items())[0])
